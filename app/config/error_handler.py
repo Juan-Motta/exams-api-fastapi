778bbe31219f07b6ccec917181b915e43f176576
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 15:59:16 -0500

    Added custom error handler for validations in pydantic shcemas

--- app/config/error_handler.py
+++ app/config/error_handler.py
@@ -1,7 +1,15 @@
+from pydantic import PydanticValueError
+
 class ApiError(ValueError):
-    
     code: int = 400
     
     def __init__(self, message: str, code: int = 400):
         super().__init__(message)
         self.code = code
+        
+
+class ValidationError(PydanticValueError):
+    msg_template = "Validation error"
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(**kwargs)
