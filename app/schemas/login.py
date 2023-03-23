574928656c90089bc2410896267d0a1005757660
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 15:59:49 -0500

    Updated login schema with the ValidationError custom class

--- app/schemas/login.py
+++ app/schemas/login.py
@@ -1,7 +1,8 @@
 import re
 from typing import Dict, Optional
 from pydantic import BaseModel, validator
-from pydantic import PydanticValueError
+
+from app.config.error_handler import ValidationError
 
 class LoginSchema(BaseModel):
     email: Optional[str]
@@ -10,14 +11,14 @@ class LoginSchema(BaseModel):
     @validator("email", always=True)
     def validate_email(cls, value: str, values: Dict, **kwargs) -> str:
         if not value:
-            raise PydanticValueError(msg_template="email is requiered")
+            raise ValidationError(email="email is requiered")
         regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
         if not re.match(regex, value):
-            raise PydanticValueError(msg_template="invalid email")
+            raise ValidationError(email="invalid email")
         return value
     
     @validator("password", always=True)
     def validate_password(cls, value: str, values: Dict, **kwargs) -> str:
         if not value:
-            raise PydanticValueError(msg_template="password is required")
+            raise ValidationError(password="password is required")
         return value
