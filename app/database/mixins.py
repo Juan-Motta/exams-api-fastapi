94e96fd44255637d390fc76c2559b62640fb6692
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 10:55:20 -0500

    Added mixin class for timestamp fields

---
+++ app/database/mixins.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from sqlalchemy import Column, DateTime, Boolean
+
+class TimestampMixin:
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    active = Column(Boolean, default=True)
