15c0ae9b5f8045147e968e2089d20fc4eb060c66
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 16:00:04 -0500

    Updated user schema with the ValidationError custom class

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,6 +2,7 @@ import re
 from typing import Dict, Optional
 from pydantic import BaseModel, validator, root_validator, PydanticValueError
 
+from app.config.error_handler import ValidationError
 from app.utils.password_hash import hash_password
 
 
@@ -15,27 +16,28 @@ class UserSchema(BaseModel):
     
     @validator("first_name", "last_name", always=True)
     def validate_name(cls, value: str, values: Dict, **kwargs) -> str:
+        field_name: str = kwargs["field"]
         field: str = kwargs["field"].name.replace("_", " ")
         if not value:
-            raise PydanticValueError(msg_template=f"{field} is required")
+            raise ValidationError({field_name:f"{field} is required"})
         pattern = r"(?:.*[\d\W_].*)"
         if re.fullmatch(pattern, value):
-            raise PydanticValueError(msg_template=f"invalid {field}")
+            raise ValidationError({field_name:f"invalid {field}"})
         return value
     
     @validator("email", always=True)
     def validate_email(cls, value: str, values: Dict, **kwargs) -> str:
         if not value:
-            raise PydanticValueError(msg_template="email is required")
+            raise ValidationError(email="email is required")
         pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
         if not re.match(pattern, value):
-            raise PydanticValueError(msg_template="invalid email")
+            raise ValidationError(email="invalid email")
         return value
     
     @validator("password", "password_confirmation", always=True)
     def validate_password(cls, value: str, values: Dict, **kwargs) -> str:
         if not value:
-            raise PydanticValueError(msg_template="password is required")
+            raise ValidationError(password="password is required")
         return value
     
     @root_validator
@@ -43,7 +45,7 @@ class UserSchema(BaseModel):
         pw1: Optional[str] = values.get("password") 
         pw2: Optional[str] = values.get("password_confirmation")
         if pw1 is not None and pw2 is not None and pw1 != pw2:
-            raise PydanticValueError(msg_template="passwords do not match")
+            raise ValidationError(password="passwords do not match")
         hashed_password: str = hash_password(pw1)
         values["password"] = hashed_password
         values["password_confirmation"] = hashed_password
