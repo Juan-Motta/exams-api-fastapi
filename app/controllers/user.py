d84fd9adc5ea94372e44a8cff713fe471593b7ac
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 15:31:14 -0500

    Modified create user controller response, now the structure is defined in the controller istead of the service

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -5,6 +5,14 @@ from sqlalchemy.orm import Session
 from app.schemas.user import UserSchema
 from app.database.sessions import get_db
 from app.services.user import create_user_service
+from app.models.user import UserModel
 
 def create_user_controller(request: UserSchema, db: Session = Depends(get_db)) -> Response:
-    return create_user_service(db, request)
+    user: UserModel = create_user_service(db, request)
+    return {
+        "id": user.id,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email,
+        "active": user.active
+    }
