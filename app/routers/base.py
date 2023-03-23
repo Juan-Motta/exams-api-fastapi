19d92ee26bf262231acc7503c1d9bb33b04805ba
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 15:31:28 -0500

    Added login route

--- app/routers/base.py
+++ app/routers/base.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from app.controllers.app import app_base_controller
+from app.controllers.login import login_controller
 
 router: APIRouter = APIRouter(
     prefix="/v1"
 )
 
 router.get("")(app_base_controller)
+router.post("/login")(login_controller)
