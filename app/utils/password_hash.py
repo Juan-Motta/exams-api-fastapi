c645b6deb4cb513f5bbee4d897755b63b3bcbf2b
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 14:31:37 -0500

    Fix error in has function related to a bad casting of the salt and password string

--- app/utils/password_hash.py
+++ app/utils/password_hash.py
@@ -4,13 +4,11 @@ import binascii
 from app.config.enviroments import CONFIG
 
 def hash_password(password: str) -> str:
-    salt = CONFIG["secret_key"]  
-    dk = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000)
+    salt = CONFIG["secret_key"]
+    dk = hashlib.pbkdf2_hmac('sha256', password.encode(), salt.encode(), 100_000)
     hashed_password = binascii.hexlify(dk)
-    return f'{salt.hex()}:{hashed_password.decode()}'
+    return f"pbkdf2_sha256$100000${hashed_password.decode()}"
 
 def verify_password(stored_password: str, provided_password: str) -> bool:
-    salt, hashed_password = stored_password.split(':')
-    salt = bytes.fromhex(salt)
-    hashed_provided_password = hash_password(provided_password, salt)
-    return hashed_password == hashed_provided_password.split(':')[1]
+    hashed_provided_password = hash_password(provided_password)
+    return stored_password == hashed_provided_password
