4d26da4170010ce6555594c336d51e08f9f6dd30
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 20:01:35 -0500

    Added update schema for users

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -6,8 +6,7 @@ from app.config.error_handler import ValidationError
 from app.utils.password_hash import hash_password
 
 
-class UserSchema(BaseModel):
-    id: Optional[int]
+class UserCreateSchema(BaseModel):
     first_name: Optional[str]
     last_name: Optional[str]
     email: Optional[str]
@@ -50,3 +49,61 @@ class UserSchema(BaseModel):
         values["password"] = hashed_password
         values["password_confirmation"] = hashed_password
         return values
+    
+
+class UserUpdateSchema(BaseModel):
+    first_name: Optional[str]
+    last_name: Optional[str]
+    email: Optional[str]
+    password: Optional[str]
+    password_confirmation: Optional[str]
+    
+    @validator("first_name", "last_name", always=True)
+    def validate_name(cls, value: str, values: Dict, **kwargs) -> str:
+        field_name: str = kwargs["field"]
+        field: str = kwargs["field"].name.replace("_", " ")
+        if not value:
+            return value
+        pattern = r"(?:.*[\d\W_].*)"
+        if re.fullmatch(pattern, value):
+            raise ValidationError({field_name:f"invalid {field}"})
+        return value
+    
+    @validator("email", always=True)
+    def validate_email(cls, value: str, values: Dict, **kwargs) -> str:
+        if not value:
+            return value
+        pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        if not re.match(pattern, value):
+            raise ValidationError(email="invalid email")
+        return value
+    
+    @validator("password", "password_confirmation", always=True)
+    def validate_password(cls, value: str, values: Dict, **kwargs) -> str:
+        if not value:
+            return value
+        return value
+    
+    @root_validator
+    def validate_passwords_match(cls, values: Dict) -> Dict:
+        pw1: Optional[str] = values.get("password") 
+        pw2: Optional[str] = values.get("password_confirmation")
+        if pw1 is None or pw2 is None:
+            return values
+        if pw1 != pw2:
+            raise ValidationError(password="passwords do not match")
+        hashed_password: str = hash_password(pw1)
+        values["password"] = hashed_password
+        values["password_confirmation"] = hashed_password
+        return values
+
+
+class UserBaseSchema(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    active: bool
+
+    class Config:
+        orm_mode = True
