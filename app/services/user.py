e34ad9f1a8bed453fde8aae9832d5c470058615b
Juan Andres Lopez Motta <juan.lopez@smartquick.com.co>
2023-03-23 20:01:52 -0500

    Added update service for users

--- app/services/user.py
+++ app/services/user.py
@@ -1,10 +1,11 @@
 from typing import Dict
 from sqlalchemy.orm.session import Session
 
-from app.schemas.user import UserSchema
+from app.schemas.user import UserCreateSchema, UserUpdateSchema
 from app.models.user import UserModel
+from app.config.error_handler import ApiError
 
-def create_user_service(db: Session, request: UserSchema) -> Dict:
+def create_user_service(db: Session, request: UserCreateSchema) -> Dict:
     user: UserModel = UserModel(
         first_name=request.first_name,
         last_name=request.last_name,
@@ -15,3 +16,18 @@ def create_user_service(db: Session, request: UserSchema) -> Dict:
     db.commit()
     db.refresh(user)
     return user
+
+def update_user_service(db: Session, request: UserUpdateSchema, id: int) -> Dict:
+    user: UserModel = db.query(UserModel).filter(UserModel.id == id).first()
+    if not user:
+        raise ApiError("invalid authetication data")
+    if request.first_name:
+        user.first_name = request.first_name
+    if request.last_name:
+        user.last_name = request.last_name
+    if request. email:
+        user.email = request.email
+    if request.password:
+        user.password = request.password
+    return user
+    
